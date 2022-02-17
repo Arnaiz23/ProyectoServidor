12985212c7d9e34c99de8e6651487bc8cdae2b53
Adrian <aaf110501@gmail.com>
2022-02-17 09:49:52 +0100

    En proceso del login

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,20 +4,45 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; //para renderizar
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Response;
+/**
+ * @Route("/")
+ */
 class DefaultController extends AbstractController //para renderizar
 {
+
     /**
-     * @Route("/", name="login")
+     * @Route("/", name="login", methods={"GET"})
      */
+    
     public function listAction()
     {
         // return $this->redirectToRoute('home');
         // ! Descomentar cuando ya se pueda
-        setcookie("session","yes");
+        // setcookie("session","yes");
         
         if(isset($_COOKIE["session"]) && $_COOKIE["session"] == "yes") return $this->redirectToRoute('home');
         return $this->render('login/index.html.twig');
+    }
+
+    /**
+     * @Route("/comprobar", name="comprobar", methods={"POST"})
+     */
+    
+    public function comprobar()
+    {
+        
+        return $this->render('login/index.html.twig');
+    }
+
+    /**
+     * @Route("/registro", name="registro", methods={"GET", "POST"})
+     */
+    
+    public function registro()
+    {
+        
+        return $this->render('register/index.html.twig');
     }
 }
 ?>
